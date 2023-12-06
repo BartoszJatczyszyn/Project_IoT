55a97b88a541d4eaae63405923897e7daf9162e1
Konrad Boś <mrkondzioU@gmail.com>
2023-12-06 01:42:31 +0100

    Implemented armAll unarmAll getAlerts backend methods

--- app/src/main/java/com/example/project_iot/activities/home/Home.java
+++ app/src/main/java/com/example/project_iot/activities/home/Home.java
@@ -2,23 +2,37 @@ package com.example.project_iot.activities.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.project_iot.R;
 import com.example.project_iot.activities.authorisation.Login;
+import com.example.project_iot.database.DatabaseHelperFactory;
+import com.example.project_iot.database.IDatabaseHelper;
+import com.example.project_iot.objects.Alarm;
 import com.example.project_iot.utils.SftpHelper;
 
 
 public class Home extends AppCompatActivity {
     Button btn_logout;
 
+    private static Activity activity;
+
+    int userId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        activity = this;
+
         setContentView(R.layout.activity_home);
+
+        userId = savedInstanceState.getInt("USER_ID");
 
         btn_logout = (Button) findViewById(R.id.btn_logout);
 
@@ -33,5 +47,80 @@ public class Home extends AppCompatActivity {
     public void logout() {
         Intent intent=new Intent(this, Login.class);
         startActivity(intent);
+    }
+
+    public void armAll() {
+
+        new Thread() {
+            @Override
+            public void run() {
+
+                IDatabaseHelper idh = DatabaseHelperFactory.getMysqlDatabase();
+                if (!idh.open()) {
+                    activity.runOnUiThread(() -> {
+                        Toast.makeText(getApplicationContext(), "Nie udało się połączyć z systemem.", Toast.LENGTH_SHORT).show();
+                    });
+                    return;
+                }
+                for(int deviceId : idh.getUserDevices(userId)){
+                    idh.updateDeviceActiveStatus(deviceId, true);
+                }
+                idh.close();
+
+                activity.runOnUiThread(() -> {
+                    Toast.makeText(getApplicationContext(), "Uzbrojono wszystkie.", Toast.LENGTH_SHORT).show();
+                });
+            }
+        }.start();
+
+    }
+
+    public void unarmAll(){
+
+        new Thread() {
+            @Override
+            public void run() {
+
+                IDatabaseHelper idh = DatabaseHelperFactory.getMysqlDatabase();
+                if (!idh.open()) {
+                    activity.runOnUiThread(() -> {
+                        Toast.makeText(getApplicationContext(), "Nie udało się połączyć z systemem.", Toast.LENGTH_SHORT).show();
+                    });
+                    return;
+                }
+                for(int deviceId : idh.getUserDevices(userId)){
+                    idh.updateDeviceActiveStatus(deviceId, false);
+                }
+                idh.close();
+
+                activity.runOnUiThread(() -> {
+                    Toast.makeText(getApplicationContext(), "Rozbrojono wszystkie.", Toast.LENGTH_SHORT).show();
+                });
+            }
+        }.start();
+    }
+
+    public void getAlerts() {
+
+        new Thread() {
+            @Override
+            public void run() {
+
+                IDatabaseHelper idh = DatabaseHelperFactory.getMysqlDatabase();
+                if (!idh.open()) {
+                    activity.runOnUiThread(() -> {
+                        Toast.makeText(getApplicationContext(), "Nie udało się połączyć z systemem.", Toast.LENGTH_SHORT).show();
+                    });
+                    return;
+                }
+
+                for (Alarm alarm : idh.getAlarmsWithStatus(userId, Alarm.Status.ACTIVE.name())){
+                    //TODO: add to alert menu
+                }
+
+                idh.close();
+            }
+        }.start();
+
     }
 }
